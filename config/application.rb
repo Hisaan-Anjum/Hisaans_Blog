83095ea7c4753c06c9eea0451cd817b8fc633032
Hisaan-Anjum <the-cuboid@hotmail.com>
2016-06-23 19:43:07 +0300

    try4

--- config/application.rb
+++ config/application.rb
@@ -25,3 +25,4 @@ module SampleApp
   end
 
 end
+  config.assets.initialize_on_precompile = false
--- config/environment.rb
+++ config/environment.rb
@@ -2,6 +2,5 @@
 require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
-  config.assets.initialize_on_precompile = false
 
 Rails.application.initialize!
