8ece2a29266dcf35ab30db8085b5b7d8a66c7ccb
Hisaan-Anjum <the-cuboid@hotmail.com>
2016-06-24 22:41:06 +0300

    google_verfication_2

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,6 @@ Rails.application.routes.draw do
    end
  end
    root "pages#home"
-   get "googlev" => "pages#googlev"
 mount Commontator::Engine => '/commontator'
 
   # The priority is based upon order of creation: first created -> highest priority.
